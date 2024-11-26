8f94ffd3d669609478a986db988686a620a35e5b
kazuki0205 <koidakazuki0505@gmail.com>
2024-11-27 02:05:23 +0900

    add: 開発環境の構築

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    rules: {
+      'react/prop-types': 'off', // TypeScriptではprop-typesは不要
+    },
+  };
+  
