f7ff594d070161690e78c2693e984dcfe33f9344
kazuki0205 <koidakazuki0505@gmail.com>
2024-11-27 02:12:52 +0900

    add: レンダリングテスト

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
