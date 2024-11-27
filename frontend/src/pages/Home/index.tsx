ddc7100efdd22140175890e60fa51f1f899264ac
kazuki0205 <koidakazuki0505@gmail.com>
2024-11-27 22:38:29 +0900

    add: Home要素のindex.tsxにTextinputコンポーネントをインポートし、検索フィールドを表示

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { useGeolocated } from 'react-geolocated';
-import GeolocationStatus from '../../components/GeolocationStatus';
+import GeolocationStatus from '../../components/GeolocationStatus'; // GeolocationStatusコンポーネントをインポート
+import TextInput from '../../components/TextInput'; // InputTextコンポーネントをインポート
 import styles from './style.module.scss'; // SCSSモジュールをインポート
+
 
 const Home: React.FC = () => {
   // useGeolocatedフックを使用して現在地を取得
@@ -16,10 +18,27 @@ const Home: React.FC = () => {
     },
     userDecisionTimeout: 5000, // ユーザーの許可を待つ時間（ミリ秒）
   });
+  
+    // 検索用の入力値と状態の管理
+    const [searchValue, setSearchValue] = React.useState('');
+
+    // 検索リセット処理
+    const handleReset = () => {
+        setSearchValue('');
+    };
+
 
   return (
     <div>
       <h1 className={styles.title}>現在地を表示</h1>
+
+      {/* 検索フィールドを表示 */}
+      <TextInput
+        value={searchValue} //現在の検索範囲
+        onChange={(value) => setSearchValue(value)} //変更時の状態関係
+        onReset={handleReset}
+        placeholder="検索キーワードを入力"
+      />
 
       {/* GeolocationStatusコンポーネントに必要な情報を渡す */}
       <GeolocationStatus
