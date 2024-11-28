473b805374f58012e991c101a59c39e1c6122e42
kazuki0205 <koidakazuki0505@gmail.com>
2024-11-28 20:25:03 +0900

    fix: serverからAPI_KEYを取得するため修正、検索結果の店舗情報を管理(データ取得に手こずった。)

--- frontend/src/api/searchRestrauntApi.ts
+++ frontend/src/api/searchRestrauntApi.ts
@@ -1,15 +1,13 @@
 import axios from "axios";
 
-const HOTPEPPER_API_URL = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/';
-const API_KEY = import.meta.env.REACT_APP_HOTPEPPER_API_KEY; //ホットペッパーのAPIキーを代入
+const  BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
 
 // 仮に現在地から300mの範囲を検索する
 
 export const searchRestrauntApi = async (lat: number, lng: number, keyword: string) => {
     try {
-      const response = await axios.get(HOTPEPPER_API_URL, {
+      const response = await axios.get(`${BACKEND_API_URL}/api/hotpepper`, {
         params: {
-          key: API_KEY, // APIキー
           lat: lat, // 緯度
           lng: lng, // 経度
           range: 1, // 検索範囲 (1: 300m / 2: 500m / 3: 1000m (初期値) / 4: 2000m / 5: 3000m
@@ -17,7 +15,18 @@ export const searchRestrauntApi = async (lat: number, lng: number, keyword: stri
           format: 'json', // レスポンス形式 json
         },
       });
-      return response.data.results.restaurant;
+
+        // データが正しく取得できたか確認し、取得したshopデータを加工して返す
+      const shops = response.data.results.shop.map((shop: any) => ({
+        id: shop.id,
+        name: shop.name,
+        lat: parseFloat(shop.lat),
+        lng: parseFloat(shop.lng),
+        address: shop.address,
+      }));
+  
+      return shops;
+
     } catch (error) {
       console.error('近くの店舗を取得できませんでした。', error);
       return [];
